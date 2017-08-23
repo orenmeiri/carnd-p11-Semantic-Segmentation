f5e07208e8ecebea6b802d35bfc521e5bc3721ac
Oren <oren@Orens-MacBook-Pro-2.local>
2017-08-23 21:03:29 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,170 @@
+import os.path
+import tensorflow as tf
+import helper
+import warnings
+from distutils.version import LooseVersion
+import project_tests as tests
+import numpy as np
+
+# Check TensorFlow Version
+assert LooseVersion(tf.__version__) >= LooseVersion('1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
+print('TensorFlow Version: {}'.format(tf.__version__))
+
+# Check for a GPU
+if not tf.test.gpu_device_name():
+    warnings.warn('No GPU found. Please use a GPU to train your neural network.')
+else:
+    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+
+
+def load_vgg(sess, vgg_path):
+    """
+    Load Pretrained VGG Model into TensorFlow.
+    :param sess: TensorFlow Session
+    :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
+    :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
+    """
+    vgg_tag = 'vgg16'
+    vgg_input_tensor_name = 'image_input:0'
+    vgg_keep_prob_tensor_name = 'keep_prob:0'
+    vgg_layer3_out_tensor_name = 'layer3_out:0'
+    vgg_layer4_out_tensor_name = 'layer4_out:0'
+    vgg_layer7_out_tensor_name = 'layer7_out:0'
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    input = tf.get_default_graph().get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = tf.get_default_graph().get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3_out = tf.get_default_graph().get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4_out = tf.get_default_graph().get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7_out = tf.get_default_graph().get_tensor_by_name(vgg_layer7_out_tensor_name)
+    drop = tf.nn.dropout()
+    return input, keep_prob, layer3_out, layer4_out, layer7_out
+tests.test_load_vgg(load_vgg, tf)
+
+def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
+    """
+    Create the layers for a fully convolutional network.  Build skip-layers using the vgg layers.
+    :param vgg_layer7_out: TF Tensor for VGG Layer 3 output
+    :param vgg_layer4_out: TF Tensor for VGG Layer 4 output
+    :param vgg_layer3_out: TF Tensor for VGG Layer 7 output
+    :param num_classes: Number of classes to classify
+    :return: The Tensor for the last layer of output
+    """
+    conv_7 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, 1, name="conv_7")
+    layer7_upscaled = tf.layers.conv2d_transpose(conv_7, num_classes, 64, 32, padding='SAME', name="layer7_upscaled")
+
+    conv_4 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, 1, name="conv_4")
+    layer4_upscaled = tf.layers.conv2d_transpose(conv_4, num_classes, 32, 16, padding='SAME', name="layer4_upscaled")
+
+    conv_3 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, 1, name="conv_3")
+    layer3_upscaled = tf.layers.conv2d_transpose(conv_3, num_classes, 16, 8, padding='SAME', name="layer3_upscaled")
+    output = tf.add(layer3_upscaled, tf.add(2 * layer4_upscaled, 4 * layer7_upscaled), name="semantic_segmentation_output")
+
+    return output
+tests.test_layers(layers)
+
+
+def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
+    """
+    Build the TensorFLow loss and optimizer operations.
+    :param nn_last_layer: TF Tensor of the last layer in the neural network
+    :param correct_label: TF Placeholder for the correct label image
+    :param learning_rate: TF Placeholder for the learning rate
+    :param num_classes: Number of classes to classify
+    :return: Tuple of (logits, train_op, cross_entropy_loss)
+    """
+    logits_flat = tf.reshape(nn_last_layer, [-1,num_classes])
+    correct_label_flat = tf.reshape(correct_label, [-1, num_classes])
+    cross_entropy_loss = tf.nn.softmax_cross_entropy_with_logits(logits=nn_last_layer, labels=correct_label_flat)#, name='xentropy'))
+    cost = tf.reduce_mean(cross_entropy_loss)
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+    return logits_flat, optimizer, cost
+tests.test_optimize(optimize)
+
+
+def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
+             correct_label, keep_prob, learning_rate, runs_dir="", data_dir="", image_shape="", logits="", saver = ""):
+    """
+    Train neural network and print out the loss during training.
+    :param sess: TF Session
+    :param epochs: Number of epochs
+    :param batch_size: Batch size
+    :param get_batches_fn: Function to get batches of training data.  Call using get_batches_fn(batch_size)
+    :param train_op: TF Operation to train the neural network
+    :param cross_entropy_loss: TF Tensor for the amount of loss
+    :param input_image: TF Placeholder for input images
+    :param correct_label: TF Placeholder for label images
+    :param keep_prob: TF Placeholder for dropout keep probability
+    :param learning_rate: TF Placeholder for learning rate
+    """
+    print("start training...")
+    print("epochs: {}".format(epochs))
+    print("batch size: {}".format(batch_size))
+
+    for epoch in range(epochs):
+        total_cost = 0
+        total_cost_count = 0
+        for images, gt_images in get_batches_fn(batch_size):
+            logins, cost = sess.run([train_op, cross_entropy_loss], feed_dict={input_image : images, correct_label: gt_images, keep_prob:0.5})
+            total_cost += cost
+            total_cost_count += 1
+        print("finished epoch {}".format(epoch))
+        print("cost: {}".format(total_cost / total_cost_count))
+        if epoch != 0 and epoch % 10 == 0:
+            helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image,
+                                          subdir='data_road/testing_some')
+        # if epoch != 0 and epoch % 5 == 0:
+        #     saver.save(sess, "semantic-segmentation", global_step = epoch)
+    print("training DONE")
+
+tests.test_train_nn(train_nn)
+
+
+def run():
+    num_classes = 2
+    image_shape = (160, 576)
+    epochs = 150
+    batch_size = 16
+    learning_rate = 0.001
+    data_dir = './data'
+    runs_dir = './runs'
+    tests.test_for_kitti_dataset(data_dir)
+
+    # Download pretrained vgg model
+    helper.maybe_download_pretrained_vgg(data_dir)
+
+    # OPTIONAL: Train and Inference on the cityscapes dataset instead of the Kitti dataset.
+    # You'll need a GPU with at least 10 teraFLOPS to train on.
+    #  https://www.cityscapes-dataset.com/
+
+    with tf.Session() as sess:
+        # Path to vgg model
+        vgg_path = os.path.join(data_dir, 'vgg')
+        # Create function to get batches
+        get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
+
+        # OPTIONAL: Augment Images for better results
+        #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
+        correct_label = tf.placeholder(tf.int32, shape=(None, image_shape[0], image_shape[1], 2))
+
+        # TODO: Build NN using load_vgg, layers, and optimize function
+        input_image, keep_prob, vgg_layer3_out, vgg_layer4_out, vgg_layer7_out = load_vgg(sess, vgg_path)
+        output = layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes)
+        logits, train_op, cross_entropy_loss = optimize(output, correct_label, learning_rate, num_classes)
+
+        sess.run(tf.global_variables_initializer())
+
+        saver = tf.train.Saver()
+
+        # TODO: Train NN using the train_nn function
+        train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image, correct_label, keep_prob, learning_rate, runs_dir, data_dir, image_shape, logits, saver)
+        saver.save(sess, "semantic-segmentation_0.5dropout", global_step = epochs)
+
+        # TODO: Save inference data using helper.save_inference_samples
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+
+        # OPTIONAL: Apply the trained model to a video
+
+
+if __name__ == '__main__':
+    run()
